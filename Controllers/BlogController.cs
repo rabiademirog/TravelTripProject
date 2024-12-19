30bc84c0b0f933564f91bba21c02887731012f45
Rabia Demiroğ <77547046+rabiademirog@users.noreply.github.com>
2024-12-19 20:34:16 +0800

    Add files via upload

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Sınıflar;
+
+
+namespace TravelTripProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c=  new Context();
+        BlogComment bc = new BlogComment();
+
+        public ActionResult Index()
+        {
+            //var bloglar = c.Blogs.ToList();
+            bc.Deger1 = c.Blogs.ToList();
+            bc.Deger3=c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+            bc.Deger4=c.Comments.OrderByDescending(x=>x.ID).Take(3).ToList();
+            return View(bc);
+        }
+
+        public ActionResult BlogDetay(int id)
+        {
+           // var blogBul = c.Blogs.Where(x => x.ID == id).ToList();
+            bc.Deger1= c.Blogs.Where(x => x.ID == id).ToList();
+            bc.Deger2=c.Comments.Where(x=>x.BlogId == id).ToList();
+            return View(bc);
+        }
+        [HttpGet]
+        public PartialViewResult DoComment(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult DoComment(Comment cm)
+        {
+            c.Comments.Add(cm);
+            c.SaveChanges();
+            return PartialView();
+        }
+        
+    }
+}
---
+++ Models/Sınıflar/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+
+namespace TravelTripProject.Models.Sınıflar
+{
+    public class Comment
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Mail { get; set; }
+        public string Commentt { get; set; }
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+}
